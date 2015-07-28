0988f5f18f53726f58784910937c75f9dac2f918
SunYu <492995897@qq.com>
2015-07-28 11:30:19 +0800

    Initial Commit

---
+++ RainChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainChat
+//
+//  Created by mackbook on 15/7/28.
+//  Copyright (c) 2015年 utaustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
